1d99b0eca53eff8aa4765118e840e605b89cbcd6
Your Name <you@example.com>
2023-01-18 21:36:10 +0400

    message

--- src/digi/Lesson.java
+++ src/digi/Lesson.java
@@ -7,10 +7,7 @@ public class Lesson {
         int[] array = {15, 10, 6, 0, 5, 0, 10, 15, 0};
 
 
-        for (int i = 0; i < array.length; i++) {
-            int a = array[i];
-            System.out.print(a + " ");
-        }
+
         System.out.println();
         for (int a : array) {
             System.out.print(a + " ");
