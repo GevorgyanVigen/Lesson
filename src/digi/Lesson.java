92b8b78fe1bafd34b68e8d5f4c659778c4e14067
Your Name <you@example.com>
2023-01-18 21:33:46 +0400

    second commit

--- src/digi/Lesson.java
+++ src/digi/Lesson.java
@@ -3,6 +3,17 @@ package digi;
 public class Lesson {
     public static void main(String[] args) {
 
-        
+
+        int[] array = {15, 10, 6, 0, 5, 0, 10, 15, 0};
+
+
+        for (int i = 0; i < array.length; i++) {
+            int a = array[i];
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int a : array) {
+            System.out.print(a + " ");
+        }
     }
 }
